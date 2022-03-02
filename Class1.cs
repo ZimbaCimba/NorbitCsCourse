8827de7aef95a5c55fea2e0dcb44c5aa92727236
ZimbaCimba <48441531+ZimbaCimba@users.noreply.github.com>
2022-03-02 19:16:13 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public static class StringLibrary
+    {
+
+        public static void WriteNumber(this int N)
+        {
+            for (int i = 1; i <= N; i++)
+                if (i < N)
+                {
+                    Console.Write(i + ", ");
+                }
+
+                else
+                {
+                    Console.Write(i + ".");
+                }
+        }
+
+        public static void WriteSqr(this int N)
+        {
+            for (var i = 0; i < N; i++)
+            {
+                for (var j = 0; j < N; j++)
+                {
+                    if ((i == N/2)&& (j == N / 2))
+                    {
+                        Console.Write("   ");
+                    }
+                    else
+                    {
+                        Console.Write(" * ");
+                    }
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
+
+            
+
+
+
+        
+
+ 
+
+
+
+
