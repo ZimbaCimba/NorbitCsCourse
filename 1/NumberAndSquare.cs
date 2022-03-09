e78058a63ce11673b9768968e9ecc0d77bf9a1f6
ZimbaCimba <48441531+ZimbaCimba@users.noreply.github.com>
2022-03-09 16:09:25 +0300

    Add files via upload

---
+++ 1/NumberAndSquare.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    /// <summary>
+    /// На основании принятых от пользователя данных производит вычисления
+    /// </summary>
+    public static class NumberAndSquare
+    {
+        /// <summary>
+        /// Выводит строку 1,2...n.
+        /// </summary>
+        /// <param name="N"></param>
+        public static void WriteNumber(this int N)
+        {
+            for (int i = 1; i <= N; i++)
+            {
+                Console.Write(
+                    (i < N)
+                        ? i + ", "
+                        : i + ".");
+            }
+        }
+        /// <summary>
+        /// Выводит квадрат из * с отверстием по середине
+        /// </summary>
+        /// <param name="N"></param>
+        public static void WriteSquare(this int N)
+        {
+            int center = N / 2;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write(
+                        (i == center && j == center)
+                         ? "   "
+                         : " * ");
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
+   
+    
+
+
+            
+
+
+
+        
+
+ 
+
+
+
+
---
+++ 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ClassLibrary;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = GetValueFromUser("Введите целое положительное число:" );
+            N.WriteNumber();
+            N = GetValueFromUser("Введите длину стороны квадрата: ");
+            N.WriteSquare();
+        }
+
+        private static int GetValueFromUser(string message)
+        {
+            Console.WriteLine(message);
+            return Convert.ToInt32(Console.ReadLine());  
+        }
+    }
+}
