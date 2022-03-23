68242dc78f8fe3a5d3784b7acef034907ba60cf5
ZimbaCimba <48441531+ZimbaCimba@users.noreply.github.com>
2022-03-23 22:05:04 +0300

    Add files via upload

---
+++ 2/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Library1
+{
+    public struct BmiArrayWord
+    {
+        public static string Bmi(double weight, double height)
+        {   
+            var bmi = Math.Round((weight / (Math.Pow(height, 2))),2);
+            return bmi.ToString();
+        }
+
+        public static string Array(string[] args)
+        {
+            int[] intArray = new int[10];
+            Random rand = new Random();
+            for (int x = 0; x < intArray.Length; x++)
+            {
+                intArray[x] = rand.Next(50);
+            }
+            Console.WriteLine("Неотсортированный массив: ");
+            foreach (int value in intArray)
+            {
+                Console.Write($"{value} ");
+            }
+            for (int i = 0; i < intArray.Length; i++)
+                for (int j = i + 1; j < intArray.Length; j++)
+                    if (intArray[i] > intArray[j])
+                    {
+                        int temp = intArray[i];
+                        intArray[i] = intArray[j];
+                        intArray[j] = temp;
+                    }
+            Console.WriteLine("\r\nОтсортированный массив: ");
+            foreach (int value in intArray)
+            {
+                Console.Write($"{value} ");
+            }
+            Console.WriteLine("\r\nМинимальное значение: " + intArray[0]);
+            Console.WriteLine("Максимальное значение массива: " + intArray[intArray.Length - 1]);
+            return intArray.ToString();
+        }
+
+        public static double Word(string str)
+        {
+            char[] arrayCharString = str.ToCharArray();
+            for (int i = 0; i <arrayCharString.Length;i++)
+            {
+                char a = arrayCharString[i];
+                if (char.IsSeparator(a) || char.IsPunctuation(a))
+                {
+                    arrayCharString[i] = ' ';
+                }
+            }
+            string result = new string(arrayCharString);
+            double averageValue = 0;
+            result = string.Join(" ", result.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+            string[] arrayString = result.Split(" ");
+            for (int i = 0; i< arrayString.Length; i++)
+            {
+                averageValue += arrayString[i].Length;
+            }
+            return Math.Round((averageValue / arrayString.Length),2);
+        }
+    }
+}
+
---
+++ 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Library1;
+
+namespace ConsoleProgram
+
+{
+    struct Program
+    {
+        static void Main(string[] args)
+        {
+            var weight = GetValueFromUser("Введите свой вес ");
+            var height = GetValueFromUser("Введите свой рост ");
+            Console.WriteLine("Ваш индкс массы тела состваляет " + BmiArrayWord.Bmi(weight, height) + "\n\r");
+            Console.WriteLine(BmiArrayWord.Array(args));
+            var str = GetStringFromUser("Введите строку: ");
+            Console.WriteLine("Средняя длина слова равна " + BmiArrayWord.Word(str) + " символов");
+        }
+            private static double GetValueFromUser(string message)
+            {
+                Console.WriteLine(message);
+                return Convert.ToDouble(Console.ReadLine());
+            }
+            private static string GetStringFromUser(string message)
+            {
+                Console.WriteLine(message);
+                return Convert.ToString(Console.ReadLine());
+            }
+    }
+}
+
