0d75d7f847e2f1cf376d778fafdb55973e571cf1
ZimbaCimba <48441531+ZimbaCimba@users.noreply.github.com>
2022-03-25 16:52:42 +0300

    Add files via upload

---
+++ 2/LibraryBmiArrayWord.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace LibraryBmiArrayWord
+{
+    public class BmiArrayWord
+    {
+        /// <summary>
+        /// Считает ИМТ
+        /// </summary>
+        /// <param name="weight"></param>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        public static string Bmi(double weight, double height)
+        {   
+            var bmi = Math.Round((weight / (Math.Pow(height, 2))),2);
+            return bmi.ToString();
+        }
+        /// <summary>
+        ///Сортирует массив, находит минимальное и максимальнео значение
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+
+        public static string Array(string[] args)
+        {
+            int[] intArray = new int[10];
+            Random rand = new Random();
+            for (int x = 0; x < intArray.Length; x++)
+            {
+                intArray[x] = rand.Next(50);
+            }
+            Console.WriteLine("Неотсортированный массив: ");
+            foreach (int value in intArray)
+            {
+                Console.Write($"{value} ");
+            }
+            for (int i = 0; i < intArray.Length; i++)
+                for (int j = i + 1; j < intArray.Length; j++)
+                    if (intArray[i] > intArray[j])
+                    {
+                        int temp = intArray[i];
+                        intArray[i] = intArray[j];
+                        intArray[j] = temp;
+                    }
+            Console.WriteLine("\r\nОтсортированный массив: ");
+            foreach (int value in intArray)
+            {
+                Console.Write($"{value} ");
+            }
+            Console.WriteLine("\r\nМинимальное значение: " + intArray[0]);
+            Console.WriteLine("Максимальное значение массива: " + intArray[intArray.Length - 1]);
+            return intArray.ToString();
+        }
+        /// <summary>
+        /// Находит среднюю длину слова в произвольной строке
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static double Word(string str)
+        {
+            char[] arrayCharString = str.ToCharArray();
+            for (int i = 0; i <arrayCharString.Length;i++)
+            {
+                char a = arrayCharString[i];
+                if (char.IsSeparator(a) || char.IsPunctuation(a))
+                {
+                    arrayCharString[i] = ' ';
+                }
+            }
+            string result = new string(arrayCharString);
+            double averageValue = 0;
+            result = string.Join(" ", result.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+            string[] arrayString = result.Split(" ");
+            for (int i = 0; i< arrayString.Length; i++)
+            {
+                averageValue += arrayString[i].Length;
+            }
+            return Math.Round((averageValue / arrayString.Length),2);
+        }
+    }
+}
+
--- 2/Program.cs
+++ 2/Program.cs
@@ -1,21 +1,32 @@
 ﻿using System;
-using Library1;
+using LibraryBmiArrayWord;
 
 namespace ConsoleProgram
 
 {
-    struct Program
+    class Program
     {
         static void Main(string[] args)
+        {
+            Bmi();
+            Console.WriteLine(BmiArrayWord.Array(args));
+            Word();
+        }
+
+        private static void Word()
+        {
+            var str = GetStringFromUser("Введите строку: ");
+            Console.WriteLine("Средняя длина слова равна " + BmiArrayWord.Word(str) + " символов");
+        }
+
+        private static void Bmi()
         {
             var weight = GetValueFromUser("Введите свой вес ");
             var height = GetValueFromUser("Введите свой рост ");
             Console.WriteLine("Ваш индкс массы тела состваляет " + BmiArrayWord.Bmi(weight, height) + "\n\r");
-            Console.WriteLine(BmiArrayWord.Array(args));
-            var str = GetStringFromUser("Введите строку: ");
-            Console.WriteLine("Средняя длина слова равна " + BmiArrayWord.Word(str) + " символов");
         }
-            private static double GetValueFromUser(string message)
+
+        private static double GetValueFromUser(string message)
             {
                 Console.WriteLine(message);
                 return Convert.ToDouble(Console.ReadLine());
